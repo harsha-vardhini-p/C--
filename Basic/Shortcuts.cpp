e3f727d577eae42746a579f7e752f9bb55b0cd0e
Harsha Vardhini <harshavardhinipillalamarri@gmail.com>
2026-01-18 13:18:22 +0530

    Boiler Plate

---
+++ Basic/Shortcuts.cpp
@@ -0,0 +1,26 @@
+//For Boiler Plate Code
+
+//Go to Settings>Snippets>cpp.json and add the following code
+
+// "Boiler Plate Code":
+/* {
+
+	 "CPP Boiler Plate": {
+	 	"prefix": "cppbp",
+	 	"body": 
+		[
+	 		"#include <iostream>",
+			"using namespace std;",
+			"int main()",
+			"{",
+	 		"$2", //Cursor Position
+            "return 0;",
+			"}",
+	 	],
+	 	"description": "C++ Boiler Plate"
+	 }
+} */
+
+/* Now, whenever you type 'cppbp' in a .cpp file, 
+it will auto-complete the boiler plate code for you. */
+
